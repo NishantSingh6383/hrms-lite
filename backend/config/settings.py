136f70865df339c3fa5884b8d9330ed9ea9604da
Nishant Singh <nishasingh@BLR.ALLEGISINDIA.COM>
2026-02-19 14:27:22 +0530

    prepare backend for deployment

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -57,11 +57,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'config.urls'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+
 
 TEMPLATES = [
     {
